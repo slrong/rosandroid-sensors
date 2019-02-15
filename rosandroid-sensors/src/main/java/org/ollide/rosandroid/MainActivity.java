ed3e3273eddcbf039c4800bfb8fe6cca6cb94da5
slrong <47650975+slrong@users.noreply.github.com>
2019-02-15 11:47:14 +0900

    upload rosandroid-sensor
    location & Imu

---
+++ rosandroid-sensors/src/main/java/org/ollide/rosandroid/MainActivity.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright (C) 2014 Oliver Degener.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ollide.rosandroid;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.ros.address.InetAddressFactory;
+import org.ros.android.RosActivity;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+
+import java.io.IOException;
+
+public class MainActivity extends RosActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText locationFrameIdView, imuFrameIdView;
+    Button applyB;
+    private OnFrameIdChangeListener locationFrameIdListener, imuFrameIdListener;
+
+    public MainActivity() {
+        super("RosAndroidExample", "RosAndroidExample");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        locationFrameIdListener = new OnFrameIdChangeListener() {
+            @Override
+            public void onFrameIdChanged(String newFrameId) {
+                Log.w(TAG, "Default location OnFrameIdChangedListener called");
+            }
+        };
+        imuFrameIdListener = new OnFrameIdChangeListener() {
+            @Override
+            public void onFrameIdChanged(String newFrameId) {
+                Log.w(TAG, "Default IMU OnFrameIdChangedListener called");
+            }
+        };
+
+        locationFrameIdView = findViewById(R.id.et_location_frame_id);
+        imuFrameIdView = findViewById(R.id.et_imu_frame_id);
+
+        SharedPreferences sp = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
+        locationFrameIdView.setText(sp.getString("locationFrameId", getString(R.string.default_location_frame_id)));
+        imuFrameIdView.setText(sp.getString("imuFrameId", getString(R.string.default_imu_frame_id)));
+
+        applyB = findViewById(R.id.b_apply);
+        applyB.setOnClickListener(this);
+    }
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+        Log.d(TAG, "init()");
+
+        final LocationPublisherNode locationPublisherNode = new LocationPublisherNode();
+        ImuPublisherNode imuPublisherNode = new ImuPublisherNode();
+
+        MainActivity.this.locationFrameIdListener = locationPublisherNode.getFrameIdListener();
+        MainActivity.this.imuFrameIdListener = imuPublisherNode.getFrameIdListener();
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        final String provider = LocationManager.GPS_PROVIDER;
+        String svcName = Context.LOCATION_SERVICE;
+        final LocationManager locationManager = (LocationManager) getSystemService(svcName);
+        final int t = 500;
+        final float distance = 0.0f;
+
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    boolean permissionFineLocation = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionCoarseLocation = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+                    Log.d(TAG, "PERMISSION 1: " + String.valueOf(permissionFineLocation));
+                    Log.d(TAG, "PERMISSION 2: " + String.valueOf(permissionCoarseLocation));
+                    if (permissionFineLocation && permissionCoarseLocation) {
+                        if (locationManager != null) {
+                            Log.d(TAG, "Requesting location");
+                            locationManager.requestLocationUpdates(provider, t, distance,
+                                    locationPublisherNode.getLocationListener());
+                            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, t, distance,
+                                    locationPublisherNode.getLocationListener());
+                        }
+                    } else {
+                        // Request permissions
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.GET_PERMISSIONS);
+                    }
+                } else {
+                    locationManager.requestLocationUpdates(provider, t, distance, locationPublisherNode.getLocationListener());
+                }
+            }
+        });
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        try {
+            Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(imuPublisherNode.getAccelerometerListener(), accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.toString());
+            return;
+        }
+
+        SensorManager sensorManager1 = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        try {
+            Sensor gyroscope = sensorManager1.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            sensorManager1.registerListener(imuPublisherNode.getGyroscopeListener(), gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.toString());
+            return;
+        }
+
+        SensorManager sensorManager2 = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        try {
+            Sensor orientation = sensorManager2.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+            sensorManager2.registerListener(imuPublisherNode.getOrientationListener(), orientation, SensorManager.SENSOR_DELAY_FASTEST);
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.toString());
+            return;
+        }
+
+        // At this point, the user has already been prompted to either enter the URI
+        // of a master to use or to start a master locally.
+
+        // The user can easily use the selected ROS Hostname in the master chooser
+        // activity.
+
+        try {
+            java.net.Socket socket = new java.net.Socket(getMasterUri().getHost(), getMasterUri().getPort());
+            java.net.InetAddress local_network_address = socket.getLocalAddress();
+            socket.close();
+            NodeConfiguration nodeConfiguration =
+                    NodeConfiguration.newPublic(local_network_address.getHostAddress(), getMasterUri());
+            nodeMainExecutor.execute(locationPublisherNode, nodeConfiguration);
+            nodeMainExecutor.execute(imuPublisherNode, nodeConfiguration);
+        } catch (IOException e) {
+            // Socket problem
+            Log.e("Camera Tutorial", "socket error trying to get networking information from the master uri");
+        }
+
+        /*
+        NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
+        nodeConfiguration.setMasterUri(getMasterUri());
+
+        nodeMainExecutor.execute(locationPublisherNode, nodeConfiguration);
+        nodeMainExecutor.execute(imuPublisherNode, nodeConfiguration);
+        */
+        onClick(null);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.i(TAG, "Default IMU OnFrameIdChangedListener called");
+
+        SharedPreferences sp = getSharedPreferences("SharedPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor spe = sp.edit();
+        String newLocationFrameId = locationFrameIdView.getText().toString();
+        if (!newLocationFrameId.isEmpty()) {
+            locationFrameIdListener.onFrameIdChanged(newLocationFrameId);
+            spe.putString("locationFrameId", newLocationFrameId);
+        }
+        String newImuFrameId = imuFrameIdView.getText().toString();
+        if (!newLocationFrameId.isEmpty()) {
+            imuFrameIdListener.onFrameIdChanged(newImuFrameId);
+            spe.putString("imuFrameId", newImuFrameId);
+        }
+        spe.apply();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PackageManager.GET_PERMISSIONS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permissions granted!");
+            } else {
+                Log.e(TAG, "Permissions not granted.");
+            }
+        }
+    }
+}
